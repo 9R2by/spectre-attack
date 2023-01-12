59858899835f92a670628b3cf93de6fa759f6aa3
9R2by <37716834+9R2by@users.noreply.github.com>
2023-01-12 03:04:26 +0100

    Update main.c

--- indirect_branch_demo/main.c
+++ indirect_branch_demo/main.c
@@ -1,12 +1,8 @@
-//inspired by https://stackoverflow.com/questions/8955209/difference-between-memory-indirect-call-and-register-indirect-call
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
 #define N 2
 
-//https://learn.microsoft.com/de-de/cpp/c-runtime-library/reference/rand?view=msvc-170
 typedef void (*fp)(void);
 
 void fn0(void){
@@ -22,19 +18,9 @@ void fn2(void){
 }
 
 int main(int argc, char** argv) {
-    //pointers to functions in array
-    //name of function indicates address of function
-    //void (*fp[]) (void) = {fn0, fn1, fn2};
     fp fp_array[] = {fn0, fn1, fn2};
-    //Seeding value not secure: not important here.
-    /* Intializes random number generator */
     srand(time(NULL));
-    //https://www.geeksforgeeks.org/generating-random-number-range-c/
     int r = rand() / (RAND_MAX / N + 1);
-    //https://learn.microsoft.com/de-de/cpp/c-runtime-library/rand-max?view=msvc-170
-    //fp fp0 = &fn0;
-   // fp fp1 = &fn1;
-  //  fp fp2 = &fn2;
     printf("%d\n", r );
     switch (r) {
         case 0:
